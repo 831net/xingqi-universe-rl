abd041cc2b1e5352b63ccfefc8373552010f4715
txqzzz <txqzzz@hotmail.com>
2019-07-07 19:21:32 +0800

    add inventory item

---
+++ components/item.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    @Author: Xingqi Tang
+    @Created: 2019/6/16 18:36
+    @Name: item
+    @Project: rl_tutorial
+"""
+class Item:
+    def __init__(self):
+        pass
--- input_handlers.py
+++ input_handlers.py
@@ -6,9 +6,19 @@
     @Project: rl_tutorial
 """
 import tcod as libtcod
+from game_states import GameStates
+
+def handle_keys(key, game_state):
+    if game_state ==  GameStates.PLAYERS_TURN:
+        return handle_player_turn_keys(key)
+    elif game_state == GameStates.PLAYER_DEAD:
+        return handle_player_dead_keys(key)
+    elif game_state == GameStates.SHOW_INVENTORY:
+        return handle_inventory_keys(key)
+    return {}
 
 
-def handle_keys(key):
+def handle_player_turn_keys(key):
     key_char = chr(key.c)
     # Movement keys
     if key.vk == libtcod.KEY_UP or key_char == 'j' or key_char == 'w':
@@ -29,23 +39,36 @@ def handle_keys(key):
         return {'move': (-1, 1)}
     elif key_char == 'n' or key_char == 'c':
         return {'move': (1, 1)}
-    # elif key.vk == libtcod.KEY_CHAR:
-    #    if key.c == ord('w'):
-    #        return {'move' : (0, -1)}
-    #    elif key.c == ord('s'):
-    #        return {'move': (0, 1)}
-    #    elif key.c == ord('a'):
-    #        return {'move': (-1, 0)}
-    #    elif key.c == ord('d'):
-    #        return {'move': (1, 0)}
 
-    # TODO: add oblique movement key control
+    if key_char =='g':
+        return {'pickup': True}
+
+    return {}
+
+def handle_player_dead_keys(key):
+    key_char = chr(key.c)
+    if key_char == 'i':
+        return {'show_inventory': True}
+
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         # Alt+Enter: toggle full screen
         return {'fullscreen': True}
     elif key.vk == libtcod.KEY_ESCAPE:
         # Exit the game
         return {'exit': True}
+    # No key was pressed
+    return {}
 
+def handle_inventory_keys(key):
+    index = key.c - ord('a')
+    if index >= 0:
+        return {'inventory_index': index}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt+Enter: toggle full screen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the game
+        return {'exit': True}
     # No key was pressed
     return {}
